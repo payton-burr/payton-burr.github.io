b691214ac2067fde90275bbd9754b30b3251bbd3
Payton <greatpayton@gmail.com>
2021-01-31 01:30:22 -0700

    changed some styles, removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Portfolio</h1>
+      <h3>New version coming soon...</h3>
     </div>
   );
 }
