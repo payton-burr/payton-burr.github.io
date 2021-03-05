ba1360275484923e95d76a063e558e847b49671f
Payton Burr <greatpayton@gmail.com>
2021-03-05 13:56:35 -0700

    Landing markup

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+function Landing() {
+  return (
+    <div>
+      <p>Hey there, I'm</p>
+      <h1>
+        Payton Burr <br />
+        I'm a developer
+      </h1>
+
+      <p>
+        I am a Front End Developer with a passion for creating cool webistes and
+        learning new things. Currently I am expanding my skills by learning Node
+        &amp; Express
+      </p>
+
+      <a>
+        <button type="button">Projects</button>
+      </a>
+    </div>
+  );
+}
+
+export default Landing;
