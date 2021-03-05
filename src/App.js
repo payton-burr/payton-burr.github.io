c9f1ef2967eedcc5412e4697267cba8b91cd6ca6
Payton Burr <greatpayton@gmail.com>
2021-03-05 13:57:44 -0700

    imported Landing into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Landing from './Components/Landing/Landing';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
 }
 
 export default App;
