99802c2302a442ef54f0fcd8751bc08a2152cc16
Payton Burr <greatpayton@gmail.com>
2021-03-05 14:15:29 -0700

    styling to Landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,24 +1,25 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
+import './styles/style.css';
 
 function Landing() {
   return (
-    <div>
-      <p>Hey there, I'm</p>
-      <h1>
+    <div className="landing">
+      <p className="landing-intro">Hey there, I'm</p>
+      <h1 className="landing-title">
         Payton Burr <br />
         I'm a developer
       </h1>
 
-      <p>
+      <p className="landing-desc">
         I am a Front End Developer with a passion for creating cool webistes and
         learning new things. Currently I am expanding my skills by learning Node
         &amp; Express
       </p>
 
-      <a>
-        <button type="button">Projects</button>
-      </a>
+      <button type="button" className="landing-btn">
+        <a>Projects</a>
+      </button>
     </div>
   );
 }
