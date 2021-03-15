c4e08f0e7f801c609618a81123fb80fafe0c83e0
payton-burr <greatpayton@gmail.com>
2021-03-14 22:09:17 -0600

    Project modifications

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles/style.css';
 
 function Project() {
   const data = {
-    name: 'Netflix Clone',
-    description: 'A complete rebuild of Netflix',
+    title: 'Netflix Clone',
+    // eslint-disable-next-line prettier/prettier
+    description:
+      'A multi-page rebuid of Netflix with authentication and protected routes. This was built using React, Firebase, Compound Components, Styled Components, and Fuse.js',
     image:
       'https://images.unsplash.com/photo-1615393457304-e0f4d3f47cd8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1946&q=80',
   };
@@ -11,7 +14,7 @@ function Project() {
     <section className="project">
       <div>
         <h3 className="project_title">{data.title}</h3>
-        <div className="">
+        <div className="project_desc-container">
           <p className="project_desc">{data.description}</p>
         </div>
       </div>
