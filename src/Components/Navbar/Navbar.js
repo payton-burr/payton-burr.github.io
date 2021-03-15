f7f914f61defe5f6da513665d1c6b41ef4321410
payton-burr <greatpayton@gmail.com>
2021-03-15 15:17:22 -0600

    refactored Nav and its styling

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,19 +5,21 @@ import './styles/style.css';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="logo">Payton</h2>
-      <ul className="navbar-nav">
-        <li className="navbar-item">
-          <a href="#" alt="Projects" className="navbar-link">
-            Projects
-          </a>
-        </li>
-        <li className="navbar-item">
-          <a href="#" alt="Contact" className="navbar-link">
-            Contact
-          </a>
-        </li>
-      </ul>
+      <div className="navbar-container">
+        <h2 className="logo">Payton</h2>
+        <ul className="navbar-nav">
+          <li className="navbar-item">
+            <a href="#" alt="Projects" className="navbar-link">
+              Projects
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="#" alt="Contact" className="navbar-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
