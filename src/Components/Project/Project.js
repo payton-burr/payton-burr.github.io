855e1607ac55086d5fadbbbeaedc6b68ef4bd095
Payton Burr <greatpayton@gmail.com>
2021-03-17 14:48:12 -0600

    styled tech and icons

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -4,13 +4,38 @@ import React from 'react';
 import './styles/style.css';
 import projectsData from '../../fixtures/projects.json';
 
-function Project({ title, description, image }) {
+function Project({ title, description, image, tech, github, preview }) {
   return (
     <div className="project">
       <div className="left">
         <h3 className="project_title">{title}</h3>
         <div className="project_desc-container">
           <p className="project_desc">{description}</p>
+
+          <div className="techstack">
+            {tech.map((item) =>
+              item.includes('svg') ? (
+                <img className="techstack_img" src={item} alt={title} />
+              ) : (
+                <p className="techstack_tech">{item}</p>
+              )
+            )}
+          </div>
+
+          <div className="project_links">
+            <a href={github}>
+              <img
+                className="project_link_img"
+                src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg"
+                alt="Github"
+              />
+            </a>
+            <a href={preview} className="project-preview-link">
+              <div style={{ fontSize: '1rem' }}>
+                <i className="fas fa-link" />
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <div className="right">
@@ -30,6 +55,9 @@ function ProjectsContainer() {
           title={item.title}
           description={item.description}
           image={item.image}
+          tech={item.tech}
+          github={item.github}
+          preview={item.preview}
         />
       ))}
     </section>
